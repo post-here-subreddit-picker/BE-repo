ce129ff0522d8087bdc77cddf4a9173a0e019985
coltynw <coltynw@users.github.com>
2020-02-03 18:55:41 -0500

    more changes

--- users/users-routes.js
+++ users/users-routes.js
@@ -11,4 +11,32 @@ router.get("/", restricted, (req, res) => {
     .catch(err => res.send(err));
 });
 
+router.get("/:id/posts", (req, res) => {
+  Post.findUserPosts(req.params.id).then(posts => {
+    if (!posts[0]) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist."
+      });
+    } else if (posts) {
+      console.log(posts);
+      res.status(200).json(posts);
+    } else
+      res.status(500).json({
+        error: "The post information could not be retrieved."
+      });
+  });
+});
+
+router.get("/dash", restricted, (req, res) => {
+  Users.findBy(req.id)
+  .then(user => {
+    return user
+  }
+  // i'm not sure how to make the logic for this.
+  )
+});
+
+
+
 module.exports = router;
+
