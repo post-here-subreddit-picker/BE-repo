2511e2be07d028ddc50c3620b1deb5bae210de78
Nicole Adams <53717338+ndacode@users.noreply.github.com>
2020-02-06 14:31:48 -0500

    Update auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -31,7 +31,7 @@ router.post("/login", (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = signToken(user);
-        res.status(200).json({ token, id });
+        res.status(200).json({ token});
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
